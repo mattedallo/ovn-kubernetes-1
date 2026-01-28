ovn-kubernetes/ovn-kubernetes

UDN controller: Add EVPN configuration translation to NAD
Extend the SpecGetter interface with GetTransport() and
GetEVPNConfiguration() methods to access EVPN fields from
ClusterUserDefinedNetwork specs.

Add renderEVPNConfig() to translate EVPN configuration from
the CUDN API to the CNI NetConf format.
Signed-off-by: Matteo Dallaglio <mdallagl@redhat.com>

go-controller/pkg/crd/userdefinednetwork/v1/spec.go
@@ -17,6 +17,16 @@ func (s *UserDefinedNetworkSpec) GetLocalnet() *LocalnetConfig {
 	return nil
 }
 
+func (s *UserDefinedNetworkSpec) GetTransport() TransportOption {
+	// UDN (namespace-scoped) does not support EVPN transport
+	return ""
+}
+
+func (s *UserDefinedNetworkSpec) GetEVPNConfiguration() *EVPNConfiguration {
+	// UDN (namespace-scoped) does not support EVPN
+	return nil
+}
+
 func (s *NetworkSpec) GetTopology() NetworkTopology {
 	return s.Topology
 }
@@ -31,4 +41,12 @@ func (s *NetworkSpec) GetLayer2() *Layer2Config {
 
 func (s *NetworkSpec) GetLocalnet() *LocalnetConfig {
 	return s.Localnet
+}
+
+func (s *NetworkSpec) GetTransport() TransportOption {
+	return s.Transport
+}
+
+func (s *NetworkSpec) GetEVPNConfiguration() *EVPNConfiguration {
+	return s.EVPNConfiguration
 }

mattedallo/ovn-kubernetes-1

go-controller/pkg/crd/userdefinednetwork/v1/spec.go
@@ -17,6 +17,16 @@ func (s *UserDefinedNetworkSpec) GetLocalnet() *LocalnetConfig {
 	return nil
 }
 
+func (s *UserDefinedNetworkSpec) GetTransport() TransportOption {
+	// UDN (namespace-scoped) does not support EVPN transport
+	return ""
+}
+
+func (s *UserDefinedNetworkSpec) GetEVPN() *EVPNConfig {
+	// UDN (namespace-scoped) does not support EVPN
+	return nil
+}
+
 func (s *NetworkSpec) GetTopology() NetworkTopology {
 	return s.Topology
 }
@@ -31,4 +41,12 @@ func (s *NetworkSpec) GetLayer2() *Layer2Config {
 
 func (s *NetworkSpec) GetLocalnet() *LocalnetConfig {
 	return s.Localnet
+}
+
+func (s *NetworkSpec) GetTransport() TransportOption {
+	return s.Transport
+}
+
+func (s *NetworkSpec) GetEVPN() *EVPNConfig {
+	return s.EVPN
 }